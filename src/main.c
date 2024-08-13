cf99f6815bb71226fe6d7c4b8a9e8eeedfc4d264
Marc Platt <plattmmarc12@gmail.com>
2024-08-13 14:46:44 -0600

    Updated .gitignore

---
+++ src/main.c
@@ -0,0 +1,10 @@
+// Executable Grammar Definition Language
+// Version 1.0
+
+#include <stdlib.h>
+#include "main.h"
+
+int dummy()
+{
+    return 3;
+}
---
+++ src/main.h
@@ -0,0 +1,6 @@
+#ifndef _H_EGDL_
+#define _H_EGDL_
+
+int dummy();
+
+#endif
